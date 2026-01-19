3dd33699baac62eff830e8ae0bf1df4f592b479f
Luã Falcão <lpjfalcao@gmail.com>
2026-01-19 16:27:52 -0300

    Criando serviço para implementar regra de desconto da venda

---
+++ src/Ambev.DeveloperEvaluation.Domain/ServicesImp/OrderService.cs
@@ -0,0 +1,55 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Services;
+
+namespace Ambev.DeveloperEvaluation.Domain.ServicesImp
+{
+    public class OrderService : IOrderService
+    {
+        public void ApplyDiscount(Order newOrder, Customer customer)
+        {
+            const string message = "Não é possível efetuar a venda de mais do que 20 itens para o produto selecionado";
+
+            var countIdenticalItemsAtDatabase = customer.Orders
+               .SelectMany(orderCustomer => orderCustomer.OrderItems)
+               .Where(orderItem => newOrder.OrderItems.Any(newOrderItem => newOrderItem.ProductId == orderItem.ProductId))
+               .Sum(x => x.Quantity);
+
+            if (countIdenticalItemsAtDatabase > 20)
+                throw new DomainException(message);
+
+            decimal discount;
+
+            foreach (var orderItem in newOrder.OrderItems)
+            {
+                if (orderItem.Quantity > 20)
+                    throw new DomainException(message);
+
+                if (customer.Orders
+                     .SelectMany(order => order.OrderItems)
+                     .Where(customerOrderItem => customerOrderItem.ProductId == orderItem.ProductId)
+                     .Any())
+                {
+                    discount = GetDiscount(countIdenticalItemsAtDatabase);
+                    orderItem.Discount = orderItem.CalculateTotalAmount() * discount;
+                    orderItem.CalculateTotalAmount(orderItem.Discount);
+                }
+                else
+                {
+                    discount = GetDiscount(orderItem.Quantity);
+                    orderItem.Discount = orderItem.CalculateTotalAmount() * discount;
+                    orderItem.CalculateTotalAmount(orderItem.Discount);
+                }
+            }
+
+            static decimal GetDiscount(int identicalItemsQuantity)
+            {
+                return identicalItemsQuantity switch
+                {
+                    int n when n >= 4 && n <= 9 => 0.1M,
+                    int n when n >= 10 && n <= 20 => 0.2M,
+                    _ => 0,
+                };
+            }
+        }
+    }
+}
